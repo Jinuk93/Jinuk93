b411826250d8a9d23449477dd6eeaca0e71661d0
Jinuk93 <rlawlsdnr430@gmail.com>
2025-07-18 22:15:50 +0900

    Delete until-github-readme-main directory

--- until-github-readme-main/src/utils/encodeImageBase64.ts
+++
@@ -1,6 +0,0 @@
-export async function encodeImageBase64(imageUrl: string) {
-  return fetch(imageUrl)
-    .then((res) => res.blob())
-    .then((blob) => blob.arrayBuffer())
-    .then((buffer) => Buffer.from(buffer).toString("base64"));
-}
--- until-github-readme-main/src/utils/formatDate.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(date: Date) {
-  // yyyy. MM. dd
-  return date.toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
-}
